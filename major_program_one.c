052dc47edf8585c516b15325605e3344c0d29f4f
Kwyx <Kwyx@users.noreply.github.com>
2015-07-02 01:41:18 -0500

    Update major_program_one.c
    My work from tonight. -Jeff

--- major_program_one.c
+++ major_program_one.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
-#include "unistd.h"
+#include <stdlib.h>
+#include <unistd.h>
 
-//************************************************************************
+////////////////////////////////////////////////////////////////////////////////
 // Program Name: major_program_one.c
 // Authors: Casey Carroll, Henry Flowers, Jeff Dunning, Matthew Holladay
 // To Compile: gcc major_program_one.c -o major_program_one.out
@@ -11,34 +12,23 @@
 // This program will act as a shell allowing input from either the user
 // or a batch file.  The program will exit upon receiving a quit command, 
 // CTRL-D, or reaching the end of the batch file.
-//***********************************************************************
+////////////////////////////////////////////////////////////////////////////////
+
 const int max_line_length = 512;
 
 void runInteractiveMode();
+void runBatchMode();
+int parseInput(char* input_line);
+void runCommand(char* input_command);
 
-int main(int argc, char *argv[]) {
-  FILE *batch_file;  //file pointer for batch file
-  char buffer[max_line_length];  //buffer to store each line from batch file
-  
+int main(int argc, char* argv[]) 
+{
   switch(argc) {
     case 1 :  //interactive mode
-      printf("Interactive Mode %s\n",argv[0]+2);
       runInteractiveMode();
       break;
     case 2 :  //batch mode
-      printf("Batch Mode\nFile - %s\n****************************\n",argv[1]);
-      
-      //MOVE THIS TO A FUNCTION***********************
-      batch_file = fopen(argv[1], "r");
-      if (!batch_file) {  //check for successful file open
-        fprintf(stderr, "Unable to open %s for reading\n",argv[1]);
-        return 1;
-      }
-      while (fgets(buffer, max_line_length, batch_file)) {
-        printf("%s",buffer);
-      }
-      fclose(batch_file);
-      //***********************
+      runBatchMode(argv[1]);
       break;
     default :  //invalid input
       fprintf(stderr,"Too many arguments\n");
@@ -47,19 +37,84 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-void runInteractiveMode () {
-  char userInput[100];
-
-  while(1)
+void runInteractiveMode () 
+{
+  char userInput[max_line_length];
+  int exit_flag = 0;
+  printf("Interactive Mode\n");
+  while(!exit_flag)
   {
     printf("prompt> ");
-    fgets(userInput, 100, stdin); //read in user input
-    strtok(userInput, "\n"); //removes newline from char array
-
-    //PARSE CHAR ARRAY
-    //FORK PROCESS FOR EACH COMMAND
-
-    printf("%s\n", userInput);
+    fgets(userInput, max_line_length, stdin); //read in user input
+    exit_flag = parseInput(userInput);
   }
 }
 
+void runBatchMode (char* file) 
+{
+  char buffer[max_line_length];  //buffer to store each line from batch file
+  FILE *batch_file;  //file pointer for batch file
+  printf("Batch Mode\nFile: %s\n",file);
+
+  batch_file = fopen(file, "r");
+  if (!batch_file)  //check for successful file open
+  {  
+    fprintf(stderr, "Unable to open %s for reading\n",file);
+    //switch to interactive mode here?
+    //return 1;
+    return;
+  }
+  while (fgets(buffer, max_line_length, batch_file)) 
+  {
+//    strtok(buffer, "\n"); //removes newline from char array
+    parseInput(buffer);
+  }
+  fclose(batch_file);
+}
+
+int parseInput(char* input_line)
+{
+  char* token_buffer;
+  int pid;
+  strtok(input_line, "\n"); //removes newline from char array
+  printf("Parse: %s\n",input_line);
+
+  token_buffer = strtok(input_line, ";");
+  if(!strcmp(token_buffer, "quit"))
+    return 1;
+  while(token_buffer != NULL)
+  {
+    pid = fork();  //fork a child to handle client requests
+    if (pid < 0) error("ERROR on fork");
+    if (pid == 0)  //child
+    {
+      printf("Child forked\n");
+      runCommand(token_buffer);
+    }
+    token_buffer = strtok(NULL, ";");
+  }
+  if (pid > 0) //parent
+    {
+      printf("Parent waiting\n");
+      wait(NULL);
+      printf("Parent resuming\n");
+    }
+  return 0;
+}
+
+void runCommand(char* input_command)  //child process
+{
+  char* args[20];  //hold the arguments
+  char** next = args;  //pointer starting at beginning of args array
+  char* token_buffer;
+  printf("Run: %s\n",input_command);
+  token_buffer = strtok(input_command, " ");
+  while(token_buffer != NULL)
+  {
+    *next++ = token_buffer;
+    token_buffer = strtok(NULL, " ");
+  }
+  *next = NULL;
+  
+  execvp(args[0], args);
+}
